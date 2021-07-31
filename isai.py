59e53a93bb979d866f64eec6506bc5ffc114a71b
daniel PRASANTH <danielprasanth@Mac-mini-de-daniel.local>
2021-07-31 22:20:42 +0200

    adding isaidub file

---
+++ isai.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""[summary] This is isai dub class
+"""
+
+
+def main():
+    """[summary]
+
+    Main method
+    """
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
